d3575088aff3d4302bf7b5f8046f482762960ce3
joybappy2 <fan328547@gmail.com>
2025-10-01 08:38:44 +0600

    project

--- src/component/Countries/Countries.jsx
+++ src/component/Countries/Countries.jsx
@@ -1,17 +1,36 @@
-import { use } from "react";
+import { use, useState } from "react";
 import Country from "../Country/Country";
 
 const Countries = ({ countriesPromise }) => {
   const countriesData = use(countriesPromise);
   const countriesArray = countriesData.countries;
-  console.log(countriesArray);
+
+  const [visitedCountry, setVisitedCountry] = useState([]);
+  const handleVisitedCountry = (country) => {
+    const newVisitedCountry = [...visitedCountry, country]
+    setVisitedCountry(newVisitedCountry)
+  }
 
   return (
-    <div className="container">
-      {countriesArray.map((country, idx) => {
-        return <Country key={idx} country={country}></Country>;
-      })}
-    </div>
+    <>
+      <h1 className="big-title">All Countries</h1>
+      <p>Total Visit: { visitedCountry.length }</p>
+      <ol>
+        {
+          visitedCountry.map( ( country, idx ) => {
+            return <div className="flex" key={ idx }>
+              <li className="lic" >{ country.name.common } </li>
+              <img className="sm-img" src={country.flags.flags.png} alt="" />
+            </div>
+          })
+        }
+      </ol>
+      <div className="container">
+        {countriesArray.map((country, idx) => {
+          return <Country key={idx} country={country} handleVisitedCountry={handleVisitedCountry}></Country>;
+        })}
+      </div>
+    </>
   );
 };
 
--- src/component/Country/Country.jsx
+++ src/component/Country/Country.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountry }) => {
+  const [visited, setVisited] = useState(false);
+
+  // console.log(visitCountHandler)
+  const visitedCountryHandler = () => {
+    const newVisited = !visited;
+    // console.log(newVisited);
+    setVisited( newVisited );
+    handleVisitedCountry(country)
+  };
+
   return (
     <div className="country-card">
       <img className="country-image" src={country.flags.flags.png} alt="" />
       <h1 className="title">Name: {country.name.common}</h1>
       <p>Population: {country.population.population}</p>
-      <p>Area: {country.area.area}</p>
+      <p>Continents: {country.continents.continents}</p>
       <div className="btn-container">
-        <button>Not Visited</button>
+        <button className={`${visited && 'visited-btn'}`} onClick={visitedCountryHandler}>
+          {visited ? "Visited" : "Not Visited"}
+        </button>
         <button>Add Flag to Visited</button>
       </div>
     </div>
